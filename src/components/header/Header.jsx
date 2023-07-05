25f3180de6797197d33cb96c21d3fe9da1bf13fd
Mohamed Reda Daghouj <mohamedredadaghouj@gmail.com>
2023-07-05 20:52:47 +0100

    first commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../images/logo.png';
 import './header.css';
 
 function Header() {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <header className='header'>
       <div className='container'>
@@ -13,29 +15,36 @@ function Header() {
         <ul>
           <a href='/'>
             <li>
-              <i class='fa-solid fa-house'></i>
+              <i className='fa-solid fa-house'></i>
               Home
             </li>
           </a>
           <a href='/'>
             <li>
-              <i class='fa-solid fa-address-card'></i>
+              <i className='fa-solid fa-address-card'></i>
               About
             </li>
           </a>
           <a href='/'>
             <li>
-              <i class='fa-solid fa-right-to-bracket'></i>
+              <i className='fa-solid fa-right-to-bracket'></i>
               Login
             </li>
           </a>
           <a href='/'>
             <li>
-              <i class='fa-solid fa-user-plus'></i>
+              <i className='fa-solid fa-user-plus'></i>
               Register
             </li>
           </a>
         </ul>
+        <div className='menu-toggle' onClick={() => setToggle(!toggle)}>
+          {toggle ? (
+            <i className='fa-solid fa-xmark'></i>
+          ) : (
+            <i className='fa-solid fa-bars'></i>
+          )}
+        </div>
       </div>
     </header>
   );
